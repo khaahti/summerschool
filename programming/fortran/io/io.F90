fb23ea2beef7318b1e4ade125b345e46080b9373
khaahti <kersti.haahti@luke.fi>
2019-06-27 10:13:43 +0300

    io exercise

--- programming/fortran/io/io.F90
+++ programming/fortran/io/io.F90
@@ -8,30 +8,27 @@ contains
 
     real, dimension(:,:), allocatable, intent(out) :: field
     character(len=*), intent(in) :: filename
+	
+	integer :: nx, ny, i
 
     ! TODO: implement function that will:
     ! open the file
-    ! read the first header line to get nx and ny
+	open(10, file=filename, status='old')
+	
+    ! read the first header line to get nx and ny (header format "# nx ny"
+	read(10, fmt='(2x,i4,1x,i4)') nx, ny
+	
     ! allocate matrix called field
+	allocate(field(nx,ny))
+	
     ! read rest of the file into field
+	do i = 1, nx
+		 read(10,*) field(i,:)
+	end do
+	
     ! close the file
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+	close(10)
+	
   end subroutine read_field
 
   ! Output routine, saves the temperature distribution as a png image
